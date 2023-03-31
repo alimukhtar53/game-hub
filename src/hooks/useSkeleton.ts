20eaa741f1c7288fe7bd990fbf70753b40a3325f
alimukhtar53 <alimukhtar52@gmail.com>
2023-03-31 22:22:52 +0500

    Code refactoring for useSkeleton hook

--- src/hooks/useSkeleton.ts
+++ src/hooks/useSkeleton.ts
@@ -1,18 +1,32 @@
 import { useBreakpointValue } from "@chakra-ui/react";
 
-function useSkeleton() {
-  const breakpoint = useBreakpointValue(
-    { base: "sm", md: "md", lg: "lg", xl: "xl" },
-    { ssr: false }
-  );
-  const skeletons =
-    breakpoint === "sm"
-      ? [1]
-      : breakpoint === "md"
-      ? [1, 2]
-      : breakpoint === "lg"
-      ? [1, 2, 3]
-      : [1, 2, 3, 4, 5];
+type BreakpointValue = "base" | "sm" | "md" | "lg" | "xl" | undefined;
+type Skeletons = number[];
+
+function useSkeleton(): { skeletons: Skeletons } {
+  const breakpoint: BreakpointValue = useBreakpointValue({
+    base: "sm",
+    sm: "sm",
+    md: "md",
+    lg: "lg",
+    xl: "xl",
+  });
+
+  const getSkeletons = (breakpoint: BreakpointValue): Skeletons => {
+    switch (breakpoint) {
+      case "sm":
+        return [1];
+      case "md":
+        return [1, 2];
+      case "lg":
+        return [1, 2, 3];
+      case "xl":
+      default:
+        return [1, 2, 3, 4, 5];
+    }
+  };
+
+  const skeletons: Skeletons = getSkeletons(breakpoint);
 
   return { skeletons };
 }
